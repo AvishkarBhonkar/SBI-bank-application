7282055162f750e0145d15284b22bff73bdc5694
AvishkarBhonkar <149956878+AvishkarBhonkar@users.noreply.github.com>
2026-01-11 10:10:04 +0530

    commit by other team

--- TestBankApplication/src/com/soft/Demo.java
+++ TestBankApplication/src/com/soft/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("welcome to java project");
 		System.out.println("Welcome to git Hub");
+		System.out.println("Welcome to Pool page");
 
 	}
 
